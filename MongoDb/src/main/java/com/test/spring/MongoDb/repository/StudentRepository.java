2bfe291463de0ea3faee8ba295722b8a003d164e
Prashant <36791956+Prashant-cristo@users.noreply.github.com>
2018-04-15 15:15:47 +0530

    Update StudentRepository.java

--- MongoDb/src/main/java/com/test/spring/MongoDb/repository/StudentRepository.java
+++ MongoDb/src/main/java/com/test/spring/MongoDb/repository/StudentRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.test.spring.MongoDb.model.Student;
 
+/**
+ * @author prkumar
+ */
+
 public interface StudentRepository extends MongoRepository<Student, String>{
 	
 	public Student findByStudentId(String studentId);
